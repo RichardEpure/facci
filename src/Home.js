7de99a366309f4a1cb0f77c58803a616d1fd9f93
Richard Epure <richardvandick@gmail.com>
2020-03-13 15:26:19 +0000

    Create homepage
    Create new component that functions as the container of all components related to the homepage.

---
+++ src/Home.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+// test imports
+import Location from "./Location";
+
+class Home extends React.Component{
+  componentDidMount(){
+   
+  }
+
+  render(){
+    return(
+      <div className="Home">
+        <Location></Location>
+      </div>
+    );
+  }
+}
+
+export default Home;
