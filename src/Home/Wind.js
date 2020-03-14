071609e50843270b2b98e851a163dc5b7df56404
Richard Epure <richardvandick@gmail.com>
2020-03-14 20:10:31 +0000

    Display wind data

---
+++ src/Home/Wind.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import "../styles/css/wind.css"
+import { getWeather } from "../Api/weather";
+
+class Wind extends React.Component
+{
+    constructor(props)
+    {
+        super(props);
+        this.state = {
+            windSpeed: "",
+        }
+    }
+
+    updateSpeed()
+    {
+        getWeather()
+        .then(weather => {
+            this.setState((state, props) => {
+                return {
+                    windSpeed: weather.windSpeed,
+                };
+            });
+        })
+        .catch(error => {
+            console.trace(error);
+        })
+    }
+
+    componentDidMount()
+    {
+        this.updateSpeed();
+    }
+
+    render()
+    {
+        return(
+            <div className="wind-container">
+                <h1>Wind</h1>
+                <h2>{this.state.windSpeed}mph</h2>
+            </div>
+        );
+    }
+
+}
+
+export default Wind;
