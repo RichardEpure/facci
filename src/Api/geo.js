d16b718c14b3a6835808606cadbaa68d127da427
Richard Epure <richardvandick@gmail.com>
2020-03-14 20:46:52 +0000

    coords return as a promise

--- src/Api/geo.js
+++ src/Api/geo.js
@@ -1,21 +1,26 @@
-export function getCoords() {
-    if(navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition( function(position) {
-            localStorage.lat = position.coords.latitude;
-            localStorage.long = position.coords.longitude;
-        });    
-    } else {
-        console.error("Geolocation is not supported by this browser!");
-    }
+export async function getCoords() {
+    return new Promise((resolve, reject) => {
+        if(navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(position => {
+                resolve(
+                    {
+                        lat: position.coords.latitude,
+                        long: position.coords.longitude,
+                    }
+                );
+            });    
+        } else {
+            console.error("Geolocation is not supported by this browser!");
+        }
+    })
 }
 
 export async function getLocation() {
-    getCoords();
-    console.log("long: " + localStorage.long);
-    const resp  = await fetch('https://reverse.geocoder.ls.hereapi.com/6.2/reversegeocode.json?prox=' + localStorage.lat + '%2C' + localStorage.long + '%2C250&mode=retrieveAddresses&maxresults=1&gen=9&apiKey=Gp4J7-0f8fxKmy6r-eyIZVMfyEHirnnR2JOoGR1SC90')
+    const coords = await getCoords();
+
+    const resp  = await fetch('https://reverse.geocoder.ls.hereapi.com/6.2/reversegeocode.json?prox=' + coords.lat + '%2C' + coords.long + '%2C250&mode=retrieveAddresses&maxresults=1&gen=9&apiKey=Gp4J7-0f8fxKmy6r-eyIZVMfyEHirnnR2JOoGR1SC90')
     .then((resp) => resp.json())
     .then((data) => {
-        console.log({data});
         return {
             "city":  data.Response.View[0].Result[0].Location.Address.City,
             "country":   data.Response.View[0].Result[0].Location.Address.State
--- src/Api/weather.js
+++ src/Api/weather.js
@@ -1,11 +1,9 @@
 import { getCoords } from "./geo";
 
 export async function getWeather() {
-    if (localStorage.lat == null || localStorage.long == null){
-        getCoords();
-    }
+    const coords = await getCoords();
 
-    const resp  = await fetch('https://api.openweathermap.org/data/2.5/weather?lat=' + localStorage.lat + '&lon=' + localStorage.long + '&appid=fe56809196ba46fa9114730ffe71f7b5')
+    const resp  = await fetch('https://api.openweathermap.org/data/2.5/weather?lat=' + coords.lat + '&lon=' + coords.long + '&appid=fe56809196ba46fa9114730ffe71f7b5')
     .then((resp) => resp.json())
     .then((data) => {
         return {
