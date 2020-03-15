495ef07bf43893b6c74905bacc48643616dc60cb
Akshit <akshit360@live.com>
2020-03-15 16:03:08 +0000

    Added Calendar

---
+++ src/Calendar/Calendar.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import Month from './Month'
+import Week from './Week'
+import '../styles/css/calendar.css'
+
+class Calendar extends Component {
+    componentDidMount()
+    {
+
+    }
+    render() { 
+        return (
+            <div className="calendarContainer"> 
+            <Month></Month>
+            <Week></Week>
+            </div>
+
+         );
+    }
+}
+ 
+export default Calendar;
