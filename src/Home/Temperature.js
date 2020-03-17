29b8dc4ee87633dd1b6b58746924fa23077c0a64
Richard Epure <richardvandick@gmail.com>
2020-03-17 20:51:41 +0000

    Add clock and current day display

--- src/Home/Temperature.js
+++ src/Home/Temperature.js
@@ -4,14 +4,28 @@ import { getWeather } from "../Api/weather";
 
 class Temperature extends React.Component
 {
+    _mounted = false;
+    _days = {
+        0: "Sunday",
+        1: "Monday",
+        2: "Tuesday",
+        3: "Wednesday",
+        4: "Thursday",
+        5: "Friday",
+        6: "Saturday",
+    }
+    _date = new Date();
+
     constructor(props)
     {
         super(props);
         this.state = {
             temperature: "",
+            time: `${this._date.getHours()}:${(this._date.getMinutes()).toString().padStart(2,'0')}`,
         }
     }
 
+    // Fetches current weather details from API and stores it in component state.
     updateTemperature()
     {
         getWeather()
@@ -27,9 +41,35 @@ class Temperature extends React.Component
         })
     }
 
+    // Constantly updates the time every passing minute.
+    updateTime()
+    {
+        const internalCallback = () => 
+        {
+            if(this._mounted)
+            {
+                this._date = new Date();
+                this.setState(() => {
+                    return {
+                        time: `${this._date.getHours()}:${(this._date.getMinutes()).toString().padStart(2,'0')}`,
+                    }
+                })
+                setTimeout(internalCallback, (60-this._date.getSeconds())*1000);
+            }
+        }
+        internalCallback();
+    }
+
     componentDidMount()
     {
+        this._mounted = true;
         this.updateTemperature();
+        this.updateTime();
+    }
+
+    componentWillUnmount()
+    {
+        this._mounted = false;
     }
 
     render()
@@ -37,6 +77,8 @@ class Temperature extends React.Component
         return(
             <div className="temperature-container">
                 <h1><span>{this.state.temperature}</span>°C</h1>
+                <h2>{this._days[this._date.getDay()]}</h2>
+                <h3>{this.state.time}</h3>
             </div>
         );
     }
