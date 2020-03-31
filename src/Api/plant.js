8b07689c87ac409544baa2980cbb2e1fecf24d96
Richard Epure <richardvandick@gmail.com>
2020-03-31 16:41:46 +0100

    change list update logic

--- src/Api/plant.js
+++ src/Api/plant.js
@@ -3,8 +3,9 @@ export var plants = [];
 
 export async function getPlants()
 {
-    const response = await fetch(`https://cors-anywhere.herokuapp.com/https://trefle.io/api/plants${token}&page_size=30`)
+    const response = await fetch(`https://cors-anywhere.herokuapp.com/https://trefle.io/api/plants${token}&page_size=100`)
         .then(data => data.json())
+        //.then(data =>  data.filter(plant => plant.complete_data === true))
         .then(data =>
             {
                 const response = data.map(plant => 
@@ -12,7 +13,8 @@ export async function getPlants()
                         return {
                             name: plant.common_name ? plant.common_name : plant.scientific_name,
                             link: plant.link,
-                            id: plant.id
+                            id: plant.id,
+                            complete_data: plant.complete_data
                         };
                     });
                 return response
@@ -27,4 +29,5 @@ export async function getPlant(link)
     const response = await fetch(`https://cors-anywhere.herokuapp.com/${link}${token}`)
         .then(data => data.json())
         .catch(error => console.error(error));
+    return response
 }
--- src/Garden/PlantList.js
+++ src/Garden/PlantList.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { plants, getPlants } from "../Api/plant";
+import { plants, getPlants, getPlant } from "../Api/plant";
 import { ArrowRightCircle as Arrow } from "react-feather";
 import "../styles/css/plantlist.css";
 
@@ -12,7 +12,9 @@ class PlantList extends React.Component
         super(props);
         this.state = {
             plants: plants,
-            elements: []
+            elements: [],
+            updateElements: false,
+            selectPlant: { id: null }
         }
     }
 
@@ -23,27 +25,55 @@ class PlantList extends React.Component
             let plants = await getPlants();
             this.setState({ 
                 plants: plants,
-                elements: this.updateList(plants)
+                updateElements: true
             });
         }
         else
         {
             this.setState({ 
-                elements: this.updateList(this.state.plants)
+                updateElements: true
             });
         }
     }
 
-    updateList(plants)
+    componentDidUpdate()
     {
-        return plants.map(plant => (
-            <li key={plant.id}>
-                <h2>{plant.name}</h2>
-                <div className="arrow">
-                    <Arrow size={this._arrowSize}></Arrow>
-                </div>
-            </li>
-        ));
+        if(this.state.updateElements)
+        {
+            this.updateElements();
+        }
+    }
+
+    async expandItem(plant)
+    {
+        let plantData = await getPlant(plant.link);
+    
+        console.log({plant: plant, plantData: plantData});
+
+        this.setState({
+            updateElements: true,
+            selectPlant: plant,
+        });
+    }
+
+    updateElements()
+    {
+        let elements = this.state.plants.map(plant => {
+                return(
+                    <li key={plant.id}>
+                        <h2>{plant.name}</h2>
+                        <div className="arrow" onClick={() => this.expandItem(plant)}>
+                            <Arrow size={this._arrowSize}></Arrow>
+                        </div>
+                    </li>
+                );
+            }
+        );
+
+        this.setState({ 
+            elements: elements,
+            updateElements: false
+        });
     }
     
     render()
@@ -51,7 +81,7 @@ class PlantList extends React.Component
         return(
             <div className="plant-list-container">
                 <ul>
-                    {this.state.elements.map(element => element)}
+                    {this.state.updateElements ? null : this.state.elements}
                 </ul>
             </div>
         );
